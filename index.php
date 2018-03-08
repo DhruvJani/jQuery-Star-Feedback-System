f22f4ee729d997fa74d7f6398feb1dbd8d597240
Dhruv Jani <dhruvjani.in@gmail.com>
2018-03-08 15:12:45 +0530

    updates on JS

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -6,6 +9,7 @@
  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600" rel="stylesheet">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="star.css" rel="stylesheet" />
+ <link rel="icon" type="image/png"  href="logo.png">
 </head>
 
 <body>
@@ -13,27 +17,37 @@
  <div class="center_align">
   <div class="demo">
    <form action="" method="post">
-    <input type="hidden" name="rating" value="0">
+    <input type="hidden" name="rating1" value="0">
+    <input type="hidden" name="rating2" value="0">
+    <input type="hidden" name="rating3" value="0">
    <div id="call_rating">
     <div class="image">
      <img src="img.jpg" width="100%">
     </div>
+    <div class="car-details">
+     <div class="car-name">
+      Classic Car
+     </div>
+     <div class="car-prise">
+      $235/Month
+     </div>
+    </div>
     <div class='rating-stars text-center'>
      Driving Quality:
       <ul class='stars'>
-        <li class='star' title='Poor' data-value='1'>
+        <li class='star' title='Poor' data-value='1' data-type='1'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Fair' data-value='2'>
+        <li class='star' title='Fair' data-value='2' data-type='1'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Good' data-value='3'>
+        <li class='star' title='Good' data-value='3' data-type='1'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Excellent' data-value='4'>
+        <li class='star' title='Excellent' data-value='4' data-type='1'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='WOW!!!' data-value='5'>
+        <li class='star' title='WOW!!!' data-value='5' data-type='1'>
           <i class='fa fa-star fa-fw'></i>
         </li>
       </ul>
@@ -41,19 +55,19 @@
     <div class='rating-stars text-center'>
      Body Condition:
       <ul class='stars'>
-        <li class='star' title='Poor' data-value='1'>
+        <li class='star' title='Poor' data-value='1' data-type='2'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Fair' data-value='2'>
+        <li class='star' title='Fair' data-value='2' data-type='2'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Good' data-value='3'>
+        <li class='star' title='Good' data-value='3' data-type='2'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Excellent' data-value='4'>
+        <li class='star' title='Excellent' data-value='4' data-type='2'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='WOW!!!' data-value='5'>
+        <li class='star' title='WOW!!!' data-value='5' data-type='2'>
           <i class='fa fa-star fa-fw'></i>
         </li>
       </ul>
@@ -61,19 +75,19 @@
     <div class='rating-stars text-center'>
      Comfort Level:
       <ul class='stars'>
-        <li class='star' title='Poor' data-value='1'>
+        <li class='star' title='Poor' data-value='1' data-type='3'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Fair' data-value='2'>
+        <li class='star' title='Fair' data-value='2' data-type='3'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Good' data-value='3'>
+        <li class='star' title='Good' data-value='3' data-type='3'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='Excellent' data-value='4'>
+        <li class='star' title='Excellent' data-value='4' data-type='3'>
           <i class='fa fa-star fa-fw'></i>
         </li>
-        <li class='star' title='WOW!!!' data-value='5'>
+        <li class='star' title='WOW!!!' data-value='5' data-type='3'>
           <i class='fa fa-star fa-fw'></i>
         </li>
       </ul>
@@ -118,6 +132,8 @@ $('.stars li').on('mouseover', function(){
 /* 2. Action to perform on click */
 $('.stars li').on('click', function(){
   var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+  var onStarType = parseInt($(this).data('type'), 10);
+
   var stars = $(this).parent().children('li.star');
   for (i = 0; i < stars.length; i++) {
     $(stars[i]).removeClass('selected');
@@ -125,15 +141,7 @@ $('.stars li').on('click', function(){
   for (i = 0; i < onStar; i++) {
     $(stars[i]).addClass('selected');
   }
-  // JUST RESPONSE (Not needed)
-  var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
-  var a = document.querySelectorAll('[data-type]');
-  for (var i in a) if (a.hasOwnProperty(i)) {
-   if(a[i].getAttribute('data-type') == 'rating'){
-    $(a[i]).remove();
-   }
-  }
-  $( "input[type=hidden][name=rating]" ).val(ratingValue);
+  $( "input[type=hidden][name=rating"+ onStarType +"]" ).val(onStar);
 });
 });
 </script>
